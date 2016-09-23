1b861958bfc2ffd02a88faf691b5ed1ecd4d1a46
oldcat <502114223@qq.com>
2016-09-23 15:04:48 +0800

    初始化提交2016年09月23日15:04:43

---
+++ app/src/main/java/com/laomao/MainActivity.java
@@ -0,0 +1,58 @@
+package com.laomao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import rx.Observable;
+import rx.Subscriber;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+        laomaoTest();
+    }
+
+    private void laomaoTest() {
+        Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("laomao");
+                subscriber.onCompleted();
+            }
+        });
+        observable.subscribe(subscriber);
+    }
+
+    Subscriber<String> subscriber = new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(String s) {
+            Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public native String stringFromJNI();
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+}
---
+++ app/src/main/java/com/laomao/beans/Laomao.java
@@ -0,0 +1,8 @@
+package com.laomao.beans;
+
+/**
+ * Created by laomao on 16/9/23.
+ */
+
+public class Laomao {
+}
