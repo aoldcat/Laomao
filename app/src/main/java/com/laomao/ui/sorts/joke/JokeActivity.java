dd46833779e74fe27c796d8cbe9a60affd9adc06
oldcat <502114223@qq.com>
2016-10-12 15:58:02 +0800

    laomao

--- app/src/main/java/com/laomao/ui/sorts/joke/JokeActivity.java
+++ app/src/main/java/com/laomao/ui/sorts/joke/JokeActivity.java
@@ -1,13 +1,15 @@
 package com.laomao.ui.sorts.joke;
 
-import android.widget.Button;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
+import com.andview.refreshview.XRefreshView;
 import com.laomao.R;
 import com.laomao.base.BaseActivity;
 import com.laomao.beans.bussiness.joke.JokeBean;
 import com.laomao.utils.SpUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -16,13 +18,18 @@ import butterknife.BindView;
  * Created by laomao on 16/10/11.
  */
 
-public class JokeActivity extends BaseActivity<JokePresenter,JokeModel> implements JokeContract.View{
+public class JokeActivity extends BaseActivity<JokePresenter, JokeModel> implements JokeContract.View {
 
-    @BindView(R.id.btn_joke_change)
-    Button btnJokeChange;
-    @BindView(R.id.tv_content)
-    TextView tvJoke;
-    int page=1;
+    @BindView(R.id.x_refreshview)
+    XRefreshView xRefreshView;
+    @BindView(R.id.recycler_view_test_rv)
+    RecyclerView recyclerView;
+    List<JokeBean.ResultBean.DataBean> jokeList;
+    JokeAdapter adapter;
+    LinearLayoutManager layoutManager;
+
+    int page = 1;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_joke;
@@ -30,22 +37,44 @@ public class JokeActivity extends BaseActivity<JokePresenter,JokeModel> implemen
 
     @Override
     public void initView() {
-        page= SpUtil.getPage();
-        btnJokeChange.setOnClickListener(view -> {
-            mPresenter.getJokes(page++,4);
-        });
-        btnJokeChange.performClick();
+        page = SpUtil.getPage();
+        jokeList = new ArrayList<>();
+        adapter = new JokeAdapter(jokeList);
+        // 设置静默加载模式
+        xRefreshView.setSilenceLoadMore();
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        // 静默加载模式不能设置footerview
+        recyclerView.setAdapter(adapter);
+        //设置刷新完成以后，headerview固定的时间
+        xRefreshView.setPinnedTime(1000);
+        xRefreshView.setMoveForHorizontal(true);
+        //设置静默加载时提前加载的item个数
+        xRefreshView.setPreLoadCount(5);
+        xRefreshView.setXRefreshViewListener(new XRefreshView.SimpleXRefreshListener() {
+            @Override
+            public void onRefresh() {
+                super.onRefresh();
+            }
 
+            @Override
+            public void onLoadMore(boolean isSilence) {
+                getJoke(5);
+            }
+        });
+        getJoke(10);
     }
 
 
     @Override
     public void getJokes(List<JokeBean.ResultBean.DataBean> jokeList) {
-        String content="";
         for(JokeBean.ResultBean.DataBean dataBean :jokeList)
-        {
-            content+=dataBean.getContent()+"\r\n\n";
-        }
-        tvJoke.setText(content);
+        adapter.insert(adapter.jokeList,dataBean,adapter.getAdapterItemCount());
+        xRefreshView.stopLoadMore();
+    }
+
+    private void getJoke(int pagezize)
+    {
+        mPresenter.getJokes(page++, pagezize);
     }
 }
